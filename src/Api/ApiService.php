f4a818f6ec5ea087b580c25977aa4bee9e7b2e24
Fabrizio Cafolla <fabrizio.cafolla97@gmail.com>
2019-06-14 16:08:08 +0200

    fix: api service

--- src/Api/ApiService.php
+++ src/Api/ApiService.php
@@ -7,9 +7,28 @@
 	use Illuminate\Support\Arr;
 	use Illuminate\Http\Request;
 
+	/**
+	 * Class ApiService
+	 * @package Kosmosx\Support\Api
+	 */
 	class ApiService extends StatusService
 	{
 		use Fractal;
+
+		/**
+		 * @param \Illuminate\Http\Request $request
+		 * @param bool                     $onlyKeys
+		 *
+		 * @return array
+		 */
+		public function getInclude(Request $request, bool $onlyKeys = false): array {
+			$query = $this->query($request, 'include');
+
+			if (array_key_exists('include', $query))
+				return $onlyKeys ? array_keys($query['include']) : $query['include'];
+
+			return array();
+		}
 
 		/**
 		 * @param \Illuminate\Http\Request $request
@@ -17,17 +36,17 @@
 		 *
 		 * @return array
 		 */
-		public function query(Request $request, $get = null):array {
-			if(null == $get || empty($get))
-				return  $request->all();
+		protected function query(Request $request, $get = null): array {
+			if (null == $get || empty($get))
+				return $request->all();
 
 			$params = array();
 
-			if(is_string($get))
-				$get = (array) $get;
+			if (is_string($get))
+				$get = (array)$get;
 
 			foreach ($get as $item)
-				if(is_string($item) && $value = $request->get($item, null))
+				if (is_string($item) && $value = $request->get($item, null))
 					$params[$item] = $value;
 
 			return $params;
@@ -39,69 +58,53 @@
 		 *
 		 * @return array
 		 */
-		public function getInclude(Request $request, bool $onlyKeys = false): array {
-			$query = $this->query($request, 'include');
-
-			if (array_key_exists('include',$query))
-				return $onlyKeys ? array_keys($query['include']) : $query['include'];
-
-			return array();
-		}
-
-		/**
-		 * @param \Illuminate\Http\Request $request
-		 * @param bool                     $onlyKeys
-		 *
-		 * @return array
-		 */
 		public function getQuery(Request $request, bool $onlyKeys = false): array {
 			$query = $this->query($request, 'query');
 
-			if (array_key_exists('query',$query))
+			if (array_key_exists('query', $query))
 				return $onlyKeys ? array_keys($query['query']) : $query['query'];
 
 			return array();
 		}
 
 		/**
-		 * JSON:API standard v1.1
+		 * Alias query method
 		 *
-		 * @param array $resource
-		 * @param array $includes
+		 * @param \Illuminate\Http\Request $request
 		 *
 		 * @return array
 		 */
-		public function manipulateResource(array $resource, array $includes = array()) {
-			$manipulate = array();
-
-			if ($data = Arr::only($resource, ['type', 'id', 'attributes', 'relationships', 'links']))
-				$manipulate['data'] = $data;
-
-			if ($included = Arr::only($resource, $includes))
-				$manipulate['included'] = $included;
-
-			return $manipulate;
+		public function getAll(Request $request) {
+			return $this->query($request);
 		}
 
 		/**
-		 * JSON:API standard v1.1
+		 * Parsa le informazioni passate nel body della richiesta
+		 * con lo standard JSON:API v1.1
 		 *
-		 * @param array $resources
-		 * @param array $includes
+		 * @param $inputs
 		 *
 		 * @return array
 		 */
-		public function manipulateResources(array $resources, array $includes = array()) {
-			$manipulate = array();
+		public function parseJsonApiRequestBody(Request $request) {
+			$data = array();
+			$inputs = $request->all();
 
-			foreach ($resources as $resource) {
-				if ($data = Arr::only($resource, ['type', 'id', 'attributes', 'relationships', 'links']))
-					$manipulate['data'][] = $data;
+			if(!array_key_exists('data', $inputs))
+				return $data;
 
-				if ($included = Arr::only($resource, $includes))
-					$manipulate['included'][$resource['id']] = $included;
+			$inputs = $inputs['data'];
+			foreach ($inputs as $key => $input) {
+				if ($key === 'attributes') {
+					foreach ($input as $itemKey => $item)
+						$data[$itemKey] = $item;
+				}
+				if ($key === 'relationships') {
+					foreach ($input as $itemKey => $item)
+						$data[$itemKey . '_id'] = $item['id'];
+				}
 			}
 
-			return $manipulate;
+			return $data;
 		}
 	}
