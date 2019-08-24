6c901921120280e290eab3ad9810b638ce75c70a
FabrizioCafolla <developer@fabriziocafolla.com>
2019-08-24 13:28:56 +0200

    init

--- src/SupportServiceProvider.php
+++ src/SupportServiceProvider.php
@@ -13,8 +13,8 @@
 		 */
 		public function register()
 		{
-			$this->app->alias(\Kosmosx\Support\Discovery\DiscoveryFacade::class, 'Discovery');
-			$this->app->alias(\Kosmosx\Support\Api\ApiFacade::class, 'Api');
+			register_alias(\Kosmosx\Support\Discovery\DiscoveryFacade::class, 'Discovery');
+			register_alias(\Kosmosx\Support\Api\ApiFacade::class, 'Api');
 
 			$this->app->bind('service.api', 'Kosmosx\Support\Api\ApiService');
 			$this->app->singleton('service.discovery', 'Kosmosx\Support\Discovery\DiscoveryService');
